1676500acf3aabe5606e7ddf86cdaec8d0d8b7f9
blitzparanoia <bluemilk44@gmail.com>
2019-08-19 19:32:48 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
   resources :companies
   resources :jobs
   resources :users
+
+  #signup
+get '/signup' => 'users#new'
+post '/signup' => 'users#create'
+#sessions
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+delete '/logout' => 'sessions#destroy'
+
+root 'sessions#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
