176f1e412d4be13288fb2d517cd65c21fe9f9189
blitzparanoia <bluemilk44@gmail.com>
2019-08-19 19:45:21 -0400

    user controller drafted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
-  def show
-  end
 
   private
 
