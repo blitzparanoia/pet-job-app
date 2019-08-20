f5ce981a6c58f24fe64886d2eeeb7f82f2b9b8b4
blitzparanoia <bluemilk44@gmail.com>
2019-08-19 20:31:48 -0400

    job create method added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,19 @@
 class JobsController < ApplicationController
 
+  def index
+  end
+
   def new
     @job = Job.new
+  end
+
+  def create
+    @job = Job.new
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
 
