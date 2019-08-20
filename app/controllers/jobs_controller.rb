ed6c63f675261d01f5cb0068050a65ba48bfbb74
blitzparanoia <bluemilk44@gmail.com>
2019-08-19 20:18:51 -0400

    job validations added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,8 @@
 class JobsController < ApplicationController
+
+  def new
+    @job = Job.new
+  end
+
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,7 @@
 class Job < ApplicationRecord
+  validates :title, presence: true
+  validates :position, presence: true
+  validates :description, presence: true
+
+  #add company via asscoiation 
 end
